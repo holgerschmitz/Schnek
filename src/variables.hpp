1d30395600cb98e8af3909dc72afcd4a39935de6
hschmitz <hschmitz@af45edbf-8987-4407-81ba-83f5d200ebee>
2012-07-30 13:17:52 +0000

    Added convenience header files
    git-svn-id: https://svn.code.sf.net/p/schnek/code/trunk@25 af45edbf-8987-4407-81ba-83f5d200ebee

---
+++ src/variables.hpp
@@ -0,0 +1,36 @@
+/*
+ * variables.hpp
+ *
+ * Created on: 30 Jul 2012
+ * Author: Holger Schmitz
+ * Email: holger@notjustphysics.com
+ *
+ * Copyright 2012 Holger Schmitz
+ *
+ * This file is part of Schnek.
+ *
+ * Schnek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Schnek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Schnek.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "variables/block.hpp"
+#include "variables/blockclasses.hpp"
+#include "variables/blockdata.hpp"
+#include "variables/blockparameters.hpp"
+#include "variables/dependencies.hpp"
+#include "variables/expression.hpp"
+#include "variables/function_expression.hpp"
+#include "variables/operators.hpp"
+#include "variables/types.hpp"
+#include "variables/variables.hpp"
