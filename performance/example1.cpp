6c8566204c44bdfcc2cff5cc2339217a13204ebf
terencerl <terence.lobo@stfc.ac.uk>
2025-05-17 08:06:13 +0100

    cleanup and removal of extra code

--- performance/example1.cpp
+++ performance/example1.cpp
@@ -6,98 +6,97 @@
  *       Email: holger@notjustphysics.com
  */
 
+ #include "../src/grid/range.hpp"
  #include "../src/grid.hpp"
+ #include "../src/grid/subgrid.hpp"
 
+ #include <iostream>
+ #include <cmath>
+ #include <chrono>
+ 
+ using namespace schnek;
+ using namespace std::chrono;
+ 
  int main()
- {
-   // Example 1
-   std::cout <<"Example 1"<<std::endl;
-   {
-     schnek::Grid<double, 2> grid(schnek::Grid<double, 2>::IndexType(5, 5));
+ {  int n  = 1000;
+
+    typedef schnek::Grid<double, 3> MyGrid;
+    typedef MyGrid::IndexType MyIndex;
  
-     grid = 1;
- 
-     for (int j = 0; j < 5; ++j)
-       grid(j, j) = sqrt(2 * j);
- 
-     for (int i = 0; i < 5; ++i)
-     {
-       for (int j = 0; j < 5; ++j)
-         std::cout << grid(i, j) << " ";
-       std::cout << std::endl;
-     }
-   }
+    MyGrid grid1(MyIndex(n, n, n));
+    MyGrid grid2(MyIndex(n, n, n));
+     
+    grid1 = 100;
+    grid2 = 100;
 
-//     //  Example 1a
-//     std::cout <<std::endl<<"Example 1a"<<std::endl;
-//    {
-//     schnek::Grid<double, 2> grid(schnek::Grid<double, 2>::IndexType(5, 5));
+    Range<int, 3>::LimitType low(0, 0, 0);
+    Range<int, 3>::LimitType high(n-1, n-1, n-1);
+    Range<int, 3> range(low, high);
 
-//     grid = 1;
+    // Case 1
+    auto beg = high_resolution_clock::now();
 
-//     for (int j = 0; j < 5; ++j)
-//       grid(j, j) = sqrt(2 * j);
-
-//     for (auto& row : grid)
-//     {
-//       for (auto& elem : row)
-//         std::cout << elem << " ";
-//       std::cout << std::endl;
-//     }
-
-    //  Example 1b
-    std::cout <<std::endl<<"Example 1b"<<std::endl;
-   {
-    schnek::Grid<double, 2> grid(schnek::Grid<double, 2>::IndexType(5, 5));
-
-    grid = 1;
-
-    for (int j = 0; j < 5; ++j)
-      grid(j, j) = sqrt(2 * j);
-
-    for (auto& elem : grid.){
-        std::cout << elem << " ";
-        // std::cout << std::endl;
+    for (auto &pos : range){
+        grid1[pos] = exp(grid1[pos]) * sin(2 * (pos[0] + pos[1] + pos[2]) * M_PI);
     }
-   }
 
-//     //  Example 1c
-//     std::cout <<std::endl<<"Example 1c"<<std::endl;
-//    {
-//     schnek::Grid<double, 2> grid(schnek::Grid<double, 2>::IndexType(5, 5));
+    auto end = high_resolution_clock::now();
 
-//     grid = 1;
+    auto duration = duration_cast<milliseconds>(end - beg);
 
-//     for (int j = 0; j < 5; ++j)
-//       grid(j, j) = sqrt(2 * j);
+    std::cout << "Case 1: Elapsed Time: " << duration.count() << "  |  " << grid1(100, 100, 100) << std::endl;
 
-//     for (int j = 0; j < 5; ++j){
-//         for (auto& elem : grid[j]){
-//             std::cout << elem << " ";
-//             // std::cout << std::endl;
-//         }
-//     }
-//    }
- 
-   // Example 2
-   std::cout <<std::endl<<"Example 2"<<std::endl;
-   {
-     typedef schnek::Grid<double, 2> MyGrid;
-     typedef MyGrid::IndexType MyIndex;
- 
-     MyGrid grid(MyIndex(5, 5));
- 
-     grid = 1;
- 
-     for (int j = 0; j < 5; ++j)
-       grid(j, j) = sqrt(2 * j);
- 
-     for (int i = 0; i < 5; ++i)
-     {
-       for (int j = 0; j < 5; ++j)
-         std::cout << grid(i, j) << " ";
-       std::cout << std::endl;
-     }
-   }
+    grid1 = 100;
+
+    // Case 2
+    beg = high_resolution_clock::now();
+
+    for (Range<int, 3>::iterator it=range.begin(); it!=range.end(); ++it)
+    {
+        const Range<int, 3>::LimitType &pos = *it;
+
+        // grid1(pos[0], pos[1], pos[2]) = exp(grid1(pos[0], pos[1], pos[2])) * sin(2 * M_PI);
+        grid1[pos] = exp(grid1[pos]) * sin(2 * (pos[0] + pos[1] + pos[2]) * M_PI);
+
+        // std::cout << grid1(pos[0], pos[1], pos[2]) << std::endl;
+    }
+
+    // std::cout << grid1(10, 10, 10) << std::endl;
+
+    end = high_resolution_clock::now();
+
+    duration = duration_cast<milliseconds>(end - beg);
+
+    std::cout << "Case 2: Elapsed Time: " << duration.count() << "  |  " << grid1(100, 100, 100) << std::endl;
+
+    // Case 3
+    beg = high_resolution_clock::now();
+
+    for (int i=grid2.getLo(0); i<=grid2.getHi(0); ++i)
+        for (int j=grid2.getLo(1); j<=grid2.getHi(1); ++j)
+            for (int k=grid2.getLo(2); k<=grid2.getHi(2); ++k){
+                grid2(i, j, k) = exp(grid2(i, j, k)) * sin(2 * (i + j + k) * M_PI);
+
+                // std::cout << grid2(i, j, k) << std::endl;
+            }
+    
+    // std::cout << grid2(10, 10, 10) << std::endl;
+
+    end = high_resolution_clock::now();
+
+    duration = duration_cast<milliseconds>(end - beg);
+
+    std::cout << "Case 3: Elapsed Time: " << duration.count() << "  |  " << grid2(100, 100, 100) << std::endl;
+
+    // for (Range<int, 2>::iterator it=range.begin(); it!=range.end(); ++it)
+    // {
+    //     const Range<int, 2>::LimitType &pos = *it;
+
+    //     grid2(pos[0], pos[1]) += grid1(pos[0], pos[1]);
+
+    //     std::cout << grid2(pos[0], pos[1]) << std::endl;
+    // }
+
+    // return
  }
  
--- performance/example2.cpp
+++ performance/example2.cpp
@@ -6,26 +6,97 @@
  *       Email: holger@notjustphysics.com
  */
 
+ #include "../src/grid/range.hpp"
  #include "../src/grid.hpp"
+ #include "../src/grid/subgrid.hpp"
+
+ #include <iostream>
  #include <cmath>
+ #include <chrono>
  
  using namespace schnek;
+ using namespace std::chrono;
  
  int main()
- {
-   typedef Grid<double, 3>::IndexType IndexType;
-   Grid<double, 3> grid(IndexType(10, 10, 10), IndexType(20, 20, 20));
+ {  int n  = 100;
+
+    typedef schnek::Grid<double, 3> MyGrid;
+    typedef MyGrid::IndexType MyIndex;
  
-   Range<int, 3> range(grid.getLo(), grid.getHi());
-   range.grow(-2);
- 
-   SubGrid<Grid<double, 3> > subGrid(range,grid);
- 
- 
-   for (int i=subGrid.getLo(0); i<=subGrid.getHi(0); ++i)
-     for (int j=subGrid.getLo(1); j<=subGrid.getHi(1); ++j)
-       for (int k=subGrid.getLo(2); k<=subGrid.getHi(2); ++k)
-         subGrid(i,j,k) = sqrt(subGrid(i,j,k));
- 
+    MyGrid grid1(MyIndex(n, n, n));
+    MyGrid grid2(MyIndex(n, n, n));
+     
+    grid1 = 100;
+    grid2 = 100;
+
+    Range<int, 3>::LimitType low(0, 0, 0);
+    Range<int, 3>::LimitType high(n-1, n-1, n-1);
+    Range<int, 3> range(low, high);
+
+    std::cout<<"Test"<<std::endl;
+
+    // Case 1
+    auto beg = high_resolution_clock::now();
+
+    for (auto &pos : range){
+        grid1[pos] = exp(grid1[pos]) * sin(2 * (pos[0] + pos[1] + pos[2]) * M_PI);
+    }
+
+    auto end = high_resolution_clock::now();
+
+    auto duration = duration_cast<milliseconds>(end - beg);
+
+    std::cout << "Case 1: Elapsed Time: " << duration.count() << "  |  " << grid1(100, 100, 100) << std::endl;
+
+    grid1 = 100;
+
+    // Case 2
+    beg = high_resolution_clock::now();
+
+    for (Range<int, 3>::iterator it=range.begin(); it!=range.end(); ++it)
+    {
+        const Range<int, 3>::LimitType &pos = *it;
+
+        // grid1(pos[0], pos[1], pos[2]) = exp(grid1(pos[0], pos[1], pos[2])) * sin(2 * M_PI);
+        grid1[pos] = exp(grid1[pos]) * sin(2 * (pos[0] + pos[1] + pos[2]) * M_PI);
+
+        // std::cout << grid1(pos[0], pos[1], pos[2]) << std::endl;
+    }
+
+    // std::cout << grid1(10, 10, 10) << std::endl;
+
+    end = high_resolution_clock::now();
+
+    duration = duration_cast<milliseconds>(end - beg);
+
+    std::cout << "Case 2: Elapsed Time: " << duration.count() << "  |  " << grid1(100, 100, 100) << std::endl;
+
+    // Case 3
+    beg = high_resolution_clock::now();
+
+    for (int i=grid2.getLo(0); i<=grid2.getHi(0); ++i)
+        for (int j=grid2.getLo(1); j<=grid2.getHi(1); ++j)
+            for (int k=grid2.getLo(2); k<=grid2.getHi(2); ++k){
+                grid2(i, j, k) = exp(grid2(i, j, k)) * sin(2 * (i + j + k) * M_PI);
+
+                // std::cout << grid2(i, j, k) << std::endl;
+            }
+    
+    // std::cout << grid2(10, 10, 10) << std::endl;
+
+    end = high_resolution_clock::now();
+
+    duration = duration_cast<milliseconds>(end - beg);
+
+    std::cout << "Case 3: Elapsed Time: " << duration.count() << "  |  " << grid2(100, 100, 100) << std::endl;
+
+    // for (Range<int, 2>::iterator it=range.begin(); it!=range.end(); ++it)
+    // {
+    //     const Range<int, 2>::LimitType &pos = *it;
+
+    //     grid2(pos[0], pos[1]) += grid1(pos[0], pos[1]);
+
+    //     std::cout << grid2(pos[0], pos[1]) << std::endl;
+    // }
  }
  
