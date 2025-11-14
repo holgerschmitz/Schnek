769a924724a272449acf1cdecfbd6e456fc3ff62
Holger Schmitz <holger@notjustphysics.com>
2025-11-14 10:28:02 +0000

    Issue #68: Fixed cstddef import for size_t

--- src/util/factor.hpp
+++ src/util/factor.hpp
@@ -28,10 +28,11 @@
 #define SCHNEK_FACTOR_HPP_
 
 #include <vector>
+#include <cstddef>
 
 namespace schnek {
 
-  void equalFactors(size_t number, size_t nfact, std::vector<size_t> &factors, std::vector<size_t> &weights);
+  void equalFactors(std::size_t number, size_t nfact, std::vector<std::size_t> &factors, std::vector<std::size_t> &weights);
 
 }  // namespace schnek
 
